a03e99d5f53acd67957d6988a8ae1ad9b3bdb697
ray.esro <ray.esro@gmail.com>
2022-06-24 17:36:27 -0500

    Added a new Student model and compontent

---
+++ BlazorTutorial/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorTutorial.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
